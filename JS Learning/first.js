a218f27dcc2f3160340f0177cd4ecc21fae1c251
Meenakshi Velip <meenakshivelip@klsvdit.edu.in>
2024-03-03 22:13:47 +0530

    first commit

---
+++ JS Learning/first.js
@@ -0,0 +1,10 @@
+console.log("Welcome to JS World")
+console.log("hello world")
+let x=50;
+console.log(x)
+x=60;
+console.log(x)
+let y = 40.40
+console.log(y)
+isCorrect = true
+console.log(isCorrect)
---
+++ JS Learning/script.js
@@ -0,0 +1,47 @@
+// // const student = {
+// //     name : "Meenakshi",
+// //     course : "BE",
+// //     address : "India"
+// // }
+// // console.log(student);
+// let guessNumber = 5;
+// let userNum = prompt("enter your number");
+// while(userNum !== guessNumber) {
+//     userNum = prompt("enter ur no");
+// }
+// console.log("congratulations, u guessed it correctly");
+// console.log("hello world");
+// console.log("hello world");
+// let a = 50;
+// let b = 60;
+// console.log("a + b = ", a+b);
+// let a = prompt("enter a value");
+// let b = prompt("enter b value");
+// console.log("a+b= ", a%b);
+// let a = prompt("enter your number");
+// if(a%2 === 0) {
+//     console.log("even number");
+// } else {
+//     console.log("not an even no")
+// }
+// let a = prompt("enter a value");
+// let b = prompt("enter b value");
+// console.log("a/b = ", a/b);
+// console.log("a*b = ", a*b);
+// console.log("a%b = ", a%b);
+// console.log("a-b = ", a-b);
+let x = prompt("enter your number");
+let rev = 0;
+let temp = x;
+let rem;
+while(x > 0) {
+    rem = x%10;
+    rev = (rev*10)+rem;
+    x = Math.floor(x/10);
+}
+if(rev == temp){
+    console.log("palindrome");
+} else {
+    console.log("not a palindrome");
+}
+
